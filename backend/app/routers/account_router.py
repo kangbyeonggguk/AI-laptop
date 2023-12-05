4ea7e56bb646ba691180d911df5d721f41e2e9a2
kangbyeonggguk <qudrnr3434@naver.com>
2023-12-05 14:54:32 +0900

    feat:소셜 로그인 추가

--- backend/app/routers/account_router.py
+++ backend/app/routers/account_router.py
@@ -8,6 +8,7 @@ from starlette import status
 from fastapi.security import OAuth2PasswordRequestForm
 from core import util
 from core.redis_config import redis_config
+import httpx
 
 
 from db.database import db
@@ -62,11 +63,62 @@ async def account_logout(refresh_token_key: str):
 
 
 
-@router.get("/accounts/login/{sns}")
-async def account_sns_login(oauth_client=Depends(util.get_oauth_client())):
-    state = secrets.token_urlsafe(32)
-    login_url = oauth_client.get_oauth_login_url(state=state)
-    return login_url
+@router.get("/accounts/login/{sns}",status_code=status.HTTP_200_OK)
+async def account_sns_login(sns: str,oauth_client=Depends(util.get_oauth_client),form_data:str=None,session: Session = Depends(db.session) ):
+   
+    if form_data is None:
+         raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect oathcode",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    else:
+        oauthcode=form_data.oauthcode
+        #redirect_uri = oauth_client(sns).redirect_uri
+        # 카카오로부터 받은 코드를 사용하여 액세스 토큰 요청
+        token_url = "https://kauth.kakao.com/oauth/token"
+        data = {
+            "grant_type": "authorization_code",
+            "client_id": oauth_client(sns).client_id,  # 여기에 카카오 디벨로퍼스에서 발급받은 클라이언트 ID를 입력
+            #"client_secret": oauth_client(sns).client_secret_id,  # 여기에 카카오 디벨로퍼스에서 발급받은 클라이언트 시크릿을 입력
+            "redirect_uri": oauth_client(sns).redirect_uri,  # 여기에 카카오 디벨로퍼스에서 설정한 리다이렉트 URI를 입력
+            "code": oauthcode,
+        }
+        platform_type
+        if sns=="kakao":
+            platform_type="K"
+        else:
+            platform_type="N"
+        try:
+            async with httpx.AsyncClient() as client:#카카오 토큰 요청
+                response = await client.post(token_url, data=data)
+                response_data = response.json()
+
+            #토큰을 받게 될 시 유저 정보 가져오기    
+            user_url = "https://kapi.kakao.com/v2/user/me"
+            headers = {"Authorization": f"Bearer {response_data['access_token']}"}
+            async with httpx.AsyncClient() as client:
+                user_response = await client.get(user_url, headers=headers)
+                user_data = user_response.json()
+
+            #유저 정보가 없다면 저장
+            check = account_crud.get_account(session, user_data.id)
+            if not check:
+                account_crud.create_account(db=session, account={"id":user_data.id,"platform_type":platform_type,"admin":0,"password":"kakao","nickname":user_data.kakao_account.name,"email":user_data.kakao_account.email,"phonenumber":user_data.kakao_acoount.phone_number})
+                
+
+            #리프레시 토큰과 액세스 토큰 발급 및 리턴
+            rd = redis_config()
+            two_weeks = 1209600
+            refresh_token = util.create_refresh_token(user_data.id)
+            rd.setex(refresh_token, two_weeks, user_data.id)
+
+            return {"access_token": util.create_access_token(user_data.id),
+            "refresh_token": refresh_token}
+        
+        except httpx.HTTPError as http_error:
+            # HTTP 요청이 실패한 경우에 대한 처리
+            return {"error": str(http_error)}
 
 
 @router.get("/callback")
