56c0ecdbcfdf5ef6d506bf06cacf72016e4c5bc1
kangbyeonggguk <qudrnr3434@naver.com>
2023-12-27 22:33:50 +0900

    feat:주석 추가

--- backend/app/routers/manager_router.py
+++ backend/app/routers/manager_router.py
@@ -13,7 +13,7 @@ from typing import Annotated,List
 router = APIRouter(
 )
 
-
+#유저 정보 가져오기
 @router.get("/admin/accounts", status_code=200)
 def read_accounts(page: int = 1, name: str = None, db: Session = Depends(db.session)):
     page_size = 7
@@ -33,7 +33,7 @@ def read_accounts(page: int = 1, name: str = None, db: Session = Depends(db.sess
 
     return {"accounts": accounts, "data_count": data_count}
 
-
+#매입 신청서 리스트 가져오기
 @router.get("/admin/laptop_sell_info_list", status_code=200)
 def read_laptop_sell_info_list(page: int = 1, name: str = None, db: Session = Depends(db.session)):
     
@@ -45,12 +45,12 @@ def read_laptop_sell_info_list(page: int = 1, name: str = None, db: Session = De
         raise HTTPException(status_code=404, detail="No admin info found")
     
     return {"list":list,"data_count":data_count}
-
+#매입 신청서 리스트 삭제
 @router.delete("/admin/laptop_sell_info_list", status_code=200)
 def delete_laptop_sell_info_list(sell_id:str=None,db: Session = Depends(db.session)):
     laptop_sell_crud.delete_laptop_sell_info_list(sell_id=sell_id,db=db)
     
-
+#노트북 정보 가져오기
 @router.get("/admin/laptop_info_list", status_code=200)
 def read_laptop_info_list(page: int = 1, name: str = None, db: Session = Depends(db.session)):
     list,data_count=laptop_info_list.get_laptops_info(page=page,name=name,db=db)
@@ -58,33 +58,39 @@ def read_laptop_info_list(page: int = 1, name: str = None, db: Session = Depends
         raise HTTPException(status_code=404, detail="No admin info found")
     return {"list":list,"data_count":data_count}
 
-
+#노트북 정보 수정
 @router.patch("/admin/laptop_info_list", status_code=200)
 def patch_laptop_info_list(info:laptop_info_list_schema.PatchLaptopinfo, db: Session = Depends(db.session)):
     laptop_info_list.patch_laptops_info(info=info,db=db)
 
+#노트북 정보 추가
 @router.post("/admin/laptop_info_list", status_code=200)
 async def create_laptop_info_list(device_name: Annotated[str, Form()],screen_size: Annotated[str, Form()],business_usage: Annotated[str, Form()],internet_lecture_usage: Annotated[str, Form()],gaming_usage: Annotated[str, Form()],purchase_limit: Annotated[str, Form()],delivery_fee: Annotated[str, Form()],manufacturing_company: Annotated[str, Form()],brand: Annotated[str, Form()],files: List[UploadFile],db: Session = Depends(db.session)):
     await laptop_info_list.create_laptops_info(device_name=device_name,screen_size=screen_size,business_usage=business_usage,internet_lecture_usage=internet_lecture_usage,gaming_usage=gaming_usage,purchase_limit=purchase_limit,delivery_fee=delivery_fee,manufacturing_company=manufacturing_company,brand=brand,files=files,db=db)
 
+#노트북 정보 삭제
 @router.delete("/admin/laptop_info_list", status_code=200)
 def delete_laptop_info_list(info_id:str=None,db: Session = Depends(db.session)):
     laptop_info_list.delete_laptops_info(info_id=info_id,db=db)
 
+#판매 노트북 정보 가져오기
 @router.get("/admin/laptop_list", status_code=200)
 def read_laptop_list(page: int = 1, name: str = None, db: Session = Depends(db.session)):
     list,total_count,info_list=laptop_info_list.get_laptop_list(page=page,name=name,db=db)
   
     return {"list":list,"data_count":total_count,"info_list":info_list}
 
+#판매 노트북 정보 수정
 @router.patch("/admin/laptop_list", status_code=200)
 def patch_laptop_list(info:laptop_schema.PatchLaptop, db: Session = Depends(db.session)):
     laptop_info_list.patch_laptop_list(info=info,db=db)
 
+#판매 노트북 정보 삭제
 @router.delete("/admin/laptop_list", status_code=200)
 def delete_laptop_list(laptop_id:str=None, db: Session = Depends(db.session)):
     laptop_info_list.delete_aptop_list(laptop_id=laptop_id,db=db)
 
+#판매 노트북 정보 추가
 @router.post("/admin/laptop_list", status_code=200)
 def create_laptop_list(info:laptop_schema.CreateLaptop,db: Session = Depends(db.session)):
     laptop_info_list.create_laptop_list(info=info,db=db)
